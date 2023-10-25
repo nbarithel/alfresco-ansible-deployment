fea70a1bea34c1b4f9dec1d683d861f8e0c2db7d
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2023-10-25 17:53:36 +0200

    more path fixes

--- roles/activemq/molecule/default/tests/test_activemq.py
+++ roles/activemq/molecule/default/tests/test_activemq.py
@@ -12,9 +12,9 @@ def get_ansible_vars(host):
     """Define get_ansible_vars"""
     java_role = "file=../java/vars/main.yml name=java_role"
     activemq_role = "file=../../vars/main.yml name=activemq_role"
-    common_vars = "file=../common/vars/main.yml name=common_vars"
-    common_defaults = "file=../common/defaults/main.yml name=common_defaults"
-    group_vars = "file=../../group_vars/all.yml name=group_vars"
+    common_vars = "file=../../../common/vars/main.yml name=common_vars"
+    common_defaults = "file=../../../common/defaults/main.yml name=common_defaults"
+    group_vars = "file=../../../../group_vars/all.yml name=group_vars"
     secrets_vars = "file=../../vars/secrets.yml name=secrets_vars"
     ansible_vars = host.ansible("include_vars", java_role)["ansible_facts"]["java_role"]
     ansible_vars.update(host.ansible("include_vars", common_defaults)["ansible_facts"]["common_defaults"])
--- roles/java/molecule/default/tests/test_java.py
+++ roles/java/molecule/default/tests/test_java.py
@@ -8,9 +8,9 @@ from hamcrest import contains_string, assert_that
 def get_ansible_vars(host):
     """Define get_ansible_vars"""
     java_role = "file=../../vars/main.yml name=java_role"
-    common_vars = "file=../common/vars/main.yml name=common_vars"
-    common_defaults = "file=../common/defaults/main.yml name=common_defaults"
-    group_vars = "file=../../group_vars/all.yml name=group_vars"
+    common_vars = "file=../../../common/vars/main.yml name=common_vars"
+    common_defaults = "file=../../../common/defaults/main.yml name=common_defaults"
+    group_vars = "file=../../../../group_vars/all.yml name=group_vars"
     ansible_vars = host.ansible(
             "include_vars",
             java_role
